fbd7a8d189cceb8ee5f1c13ee9f2d35704c5a260
Srikanth Reddy <sriors123@gmail.com>
2023-10-11 23:58:59 +0530

    tab working, landing page set

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const tabs = document.querySelectorAll('.tab');
+    const contents = document.querySelectorAll('.content');
+
+    tabs.forEach(function(tab) {
+        tab.addEventListener('click', function() {
+            const target = this.id.replace('-tab', '');
+            contents.forEach(function(content) {
+                content.classList.remove('active');
+            });
+            document.querySelector(`.${target}`).classList.add('active');
+        });
     });
-
-
-  function openTab(name){
-    
-    } 
+});
