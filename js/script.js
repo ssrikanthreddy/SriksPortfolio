b09ebb440a281ea9ff666347e99031d6138b4f11
Srikanth Reddy <sriors123@gmail.com>
2023-10-12 14:55:03 +0530

    Layout optimised, Animation working

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const tabs = document.querySelectorAll('.tab');
-    const contents = document.querySelectorAll('.content');
+  const tabs = document.querySelectorAll('.tab');
+  const contents = document.querySelectorAll('.content');
 
-    tabs.forEach(function(tab) {
-        tab.addEventListener('click', function() {
-            const target = this.id.replace('-tab', '');
-            contents.forEach(function(content) {
-                content.classList.remove('active');
-            });
-            document.querySelector(`.${target}`).classList.add('active');
-        });
+  tabs.forEach(tab => {
+    tab.addEventListener('click', function() {
+      const activeTab = document.querySelector('.tab.active');
+      const activeContent = document.querySelector('.content.active');
+      const targetContent = document.querySelector(`.${tab.id}.content`);
+
+      if (activeTab !== tab) {
+        activeTab.classList.remove('active');
+        tab.classList.add('active');
+
+        activeContent.classList.remove('active', 'roll-down');
+        activeContent.classList.add('roll-up');
+        targetContent.classList.add('active', 'roll-down');
+
+        // Triggering reflow to force the animation
+        void targetContent.offsetWidth;
+
+        activeContent.classList.remove('roll-up');
+        
+      }
     });
+  });
 });
